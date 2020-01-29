9f249c9c1193e9a416761b06fc138ce4146db53a
censhin <r.neill.johnson@gmail.com>
2020-01-28 22:41:31 -0600

    feat(all): adding initial files to bootstrap the project

---
+++ src/main/java/com/cs3365/rpgdapi/RpgdApiApplication.java
@@ -0,0 +1,13 @@
+package com.cs3365.rpgdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgdApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgdApiApplication.class, args);
+	}
+
+}
